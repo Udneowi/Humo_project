057532dfc1ffcd1a4782bc7a3d9e3b5a9dd9234e
Udneowi <s144256@student.dtu.dk>
2018-12-10 22:47:16 +0100

    Updating figures

--- common/quaternion.py
+++ common/quaternion.py
@@ -7,7 +7,6 @@
 
 import torch
 import numpy as np
-
 # PyTorch-backed implementations
 
 def qmul(q, r):
@@ -18,9 +17,9 @@ def qmul(q, r):
     """
     assert q.shape[-1] == 4
     assert r.shape[-1] == 4
-    
+
     original_shape = q.shape
-    
+
     # Compute outer product
     terms = torch.bmm(r.view(-1, 4, 1), q.view(-1, 1, 4))
 
@@ -40,11 +39,11 @@ def qrot(q, v):
     assert q.shape[-1] == 4
     assert v.shape[-1] == 3
     assert q.shape[:-1] == v.shape[:-1]
-    
+
     original_shape = list(v.shape)
     q = q.view(-1, 4)
     v = v.view(-1, 3)
-    
+
     qvec = q[:, 1:]
     uv = torch.cross(qvec, v, dim=1)
     uuv = torch.cross(qvec, uv, dim=1)
@@ -57,16 +56,16 @@ def qeuler(q, order, epsilon=0):
     Returns a tensor of shape (*, 3).
     """
     assert q.shape[-1] == 4
-    
+
     original_shape = list(q.shape)
     original_shape[-1] = 3
     q = q.view(-1, 4)
-    
+
     q0 = q[:, 0]
     q1 = q[:, 1]
     q2 = q[:, 2]
     q3 = q[:, 3]
-    
+
     if order == 'xyz':
         x = torch.atan2(2 * (q0 * q1 - q2 * q3), 1 - 2*(q1 * q1 + q2 * q2))
         y = torch.asin(torch.clamp(2 * (q1 * q3 + q0 * q2), -1+epsilon, 1-epsilon))
@@ -104,8 +103,8 @@ def qmul_np(q, r):
     return qmul(q, r).numpy()
 
 def qrot_np(q, v):
-    q = torch.from_numpy(q).contiguous()
-    v = torch.from_numpy(v).contiguous()
+    q = torch.from_numpy(q).contiguous().double()
+    v = torch.from_numpy(v).contiguous().double()
     return qrot(q, v).numpy()
 
 def qeuler_np(q, order, epsilon=0, use_gpu=False):
@@ -121,13 +120,13 @@ def qfix(q):
     Enforce quaternion continuity across the time dimension by selecting
     the representation (q or -q) with minimal distance (or, equivalently, maximal dot product)
     between two consecutive frames.
-    
+
     Expects a tensor of shape (L, J, 4), where L is the sequence length and J is the number of joints.
     Returns a tensor of the same shape.
     """
     assert len(q.shape) == 3
     assert q.shape[-1] == 4
-    
+
     result = q.copy()
     dot_products = np.sum(q[1:]*q[:-1], axis=2)
     mask = dot_products < 0
@@ -143,7 +142,7 @@ def expmap_to_quaternion(e):
     Returns a tensor of shape (*, 4).
     """
     assert e.shape[-1] == 3
-    
+
     original_shape = list(e.shape)
     original_shape[-1] = 4
     e = e.reshape(-1, 3)
@@ -158,16 +157,16 @@ def euler_to_quaternion(e, order):
     Convert Euler angles to quaternions.
     """
     assert e.shape[-1] == 3
-    
+
     original_shape = list(e.shape)
     original_shape[-1] = 4
-    
+
     e = e.reshape(-1, 3)
-    
+
     x = e[:, 0]
     y = e[:, 1]
     z = e[:, 2]
-    
+
     rx = np.stack((np.cos(x/2), np.sin(x/2), np.zeros_like(x), np.zeros_like(x)), axis=1)
     ry = np.stack((np.cos(y/2), np.zeros_like(y), np.sin(y/2), np.zeros_like(y)), axis=1)
     rz = np.stack((np.cos(z/2), np.zeros_like(z), np.zeros_like(z), np.sin(z/2)), axis=1)
@@ -186,10 +185,9 @@ def euler_to_quaternion(e, order):
             result = r
         else:
             result = qmul_np(result, r)
-            
+
     # Reverse antipodal representation to have a non-negative "w"
     if order in ['xyz', 'yzx', 'zxy']:
         result *= -1
-    
+
     return result.reshape(original_shape)
-    
---
+++ common/visualization_patrick.py
@@ -0,0 +1,241 @@
+# Copyright (c) 2018-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, writers
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import torch
+from common.quaternion import qrot
+
+def render_animation(data, skeleton, fps, output='interactive', bitrate=1000):
+    """
+    Render or show an animation. The supported output modes are:
+     -- 'interactive': display an interactive figure
+                       (also works on notebooks if associated with %matplotlib inline)
+     -- 'html': render the animation as HTML5 video. Can be displayed in a notebook using HTML(...).
+     -- 'filename.mp4': render and export the animation as an h264 video (requires ffmpeg).
+     -- 'filename.gif': render and export the animation a gif file (requires imagemagick).
+    """
+    x = 0
+    y = 1
+    z = 2
+    radius = torch.max(skeleton.offsets()).item() * 10
+
+    skeleton_parents = skeleton.parents()
+
+    plt.ioff()
+    fig = plt.figure(figsize=(4, 4))
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
+    ax.view_init(elev=20., azim=30)
+
+    ax.set_xlim3d([-radius/2, radius/2])
+    ax.set_zlim3d([0, radius])
+    ax.set_ylim3d([-radius/2, radius/2])
+    ax.set_aspect('equal')
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.set_zticklabels([])
+    ax.dist = 7.5
+
+    lines = []
+    initialized = False
+
+    trajectory = data[:, 0, [0, 2]]
+    avg_segment_length = np.mean(np.linalg.norm(np.diff(trajectory, axis=0), axis=1)) + 1e-3
+    draw_offset = int(25/avg_segment_length)
+    spline_line, = ax.plot(*trajectory.T)
+    camera_pos = trajectory
+    height_offset = np.min(data[:, :, 1]) # Min height
+    data = data.copy()
+    data[:, :, 1] -= height_offset
+
+    def update(frame):
+        nonlocal initialized
+        ax.set_xlim3d([-radius/2 + camera_pos[frame, 0], radius/2 + camera_pos[frame, 0]])
+        ax.set_ylim3d([-radius/2 + camera_pos[frame, 1], radius/2 + camera_pos[frame, 1]])
+
+        positions_world = data[frame]
+        for i in range(positions_world.shape[0]):
+            if skeleton_parents[i] == -1:
+                continue
+            if not initialized:
+                col = 'red' if i in skeleton.joints_right() else 'black' # As in audio cables :)
+                lines.append(ax.plot([positions_world[i, x], positions_world[skeleton_parents[i], x]],
+                        [positions_world[i, y], positions_world[skeleton_parents[i], y]],
+                        [positions_world[i, z], positions_world[skeleton_parents[i], z]], zdir='y', c=col))
+            else:
+                lines[i-1][0].set_xdata([positions_world[i, x], positions_world[skeleton_parents[i], x]])
+                lines[i-1][0].set_ydata([positions_world[i, y], positions_world[skeleton_parents[i], y]])
+                lines[i-1][0].set_3d_properties([positions_world[i, z], positions_world[skeleton_parents[i], z]], zdir='y')
+        l = max(frame-draw_offset, 0)
+        r = min(frame+draw_offset, trajectory.shape[0])
+        spline_line.set_xdata(trajectory[l:r, 0])
+        spline_line.set_ydata(np.zeros_like(trajectory[l:r, 0]))
+        spline_line.set_3d_properties(trajectory[l:r, 1], zdir='y')
+        initialized = True
+        if output == 'interactive' and frame == data.shape[0] - 1:
+            plt.close('all')
+
+    fig.tight_layout()
+    anim = FuncAnimation(fig, update, frames=np.arange(0, data.shape[0]), interval=1000/fps, repeat=False)
+    if output == 'interactive':
+        plt.show()
+        return anim
+    elif output == 'html':
+        return anim.to_html5_video()
+    elif output.endswith('.mp4'):
+        Writer = writers['ffmpeg']
+        writer = Writer(fps=fps, metadata={}, bitrate=bitrate)
+        anim.save(output, writer=writer)
+    elif output.endswith('.gif'):
+        anim.save(output, dpi=80, writer='imagemagick')
+    else:
+        raise ValueError('Unsupported output format (only html, .mp4, and .gif are supported)')
+    plt.close()
+
+
+def render_animation_dual(data_real, data_gen, skeleton, fps, output='interactive', bitrate=1000,zoom=False):
+    """
+    Render or show an animation. The supported output modes are:
+     -- 'interactive': display an interactive figure
+                       (also works on notebooks if associated with %matplotlib inline)
+     -- 'html': render the animation as HTML5 video. Can be displayed in a notebook using HTML(...).
+     -- 'filename.mp4': render and export the animation as an h264 video (requires ffmpeg).
+     -- 'filename.gif': render and export the animation a gif file (requires imagemagick).
+    """
+    x = 0
+    y = 1
+    z = 2
+    radius = torch.max(skeleton.offsets()).item() * 10
+
+    skeleton_parents = skeleton.parents()
+    index_for_joint = 9
+
+    plt.ioff()
+    fig = plt.figure(figsize=(8, 4))
+    ax_real = fig.add_subplot(1, 2, 1, projection='3d')
+    ax_real.view_init(elev=20., azim=30)
+
+    if zoom != False:
+        x1,x2,y1,y2,z1,z2 = zoom
+        ax_real.set_xlim3d([x1*radius, x2*radius])
+        ax_real.set_zlim3d([z1*radius, z2*radius])
+        ax_real.set_ylim3d([y1*radius, y2*radius])
+    else:
+        ax_real.set_xlim3d([-radius/2, radius/2])
+        ax_real.set_zlim3d([0, radius])
+        ax_real.set_ylim3d([-radius/2, radius/2])
+
+    ax_real.set_aspect('equal')
+    ax_real.set_xticklabels([])
+    ax_real.set_yticklabels([])
+    ax_real.set_zticklabels([])
+    ax_real.dist = 7.5
+
+    lines_real = []
+    lines_real_elbow_last_point = [[]]
+    initialized_real = False
+
+    trajectory_real = data_real[:, 0, [0, 2]]
+    avg_segment_length = np.mean(np.linalg.norm(np.diff(trajectory_real, axis=0), axis=1)) + 1e-3
+    draw_offset = int(25/avg_segment_length)
+    spline_line_real, = ax_real.plot(*trajectory_real.T)
+    height_offset_real = np.min(data_real[:, :, 1]) # Min height
+    data_real = data_real.copy()
+    data_real[:, :, 1] -= height_offset_real
+
+    ax_gen = fig.add_subplot(1, 2, 2, projection='3d')
+    ax_gen.view_init(elev=20., azim=30)
+
+    ax_gen.set_xlim3d([-radius/2, radius/2])
+    ax_gen.set_zlim3d([0, radius])
+    ax_gen.set_ylim3d([-radius/2, radius/2])
+    ax_gen.set_aspect('equal')
+    ax_gen.set_xticklabels([])
+    ax_gen.set_yticklabels([])
+    ax_gen.set_zticklabels([])
+    ax_gen.dist = 7.5
+
+    lines_gen = []
+    lines_gen_elbow_last_point = [[]]
+    initialized_gen = False
+
+    trajectory_gen = data_gen[:, 0, [0, 2]]
+    avg_segment_length = np.mean(np.linalg.norm(np.diff(trajectory_gen, axis=0), axis=1)) + 1e-3
+    draw_offset = int(25/avg_segment_length)
+    spline_line_gen, = ax_gen.plot(*trajectory_gen.T)
+    height_offset_gen = np.min(data_gen[:, :, 1]) # Min height
+    data_gen = data_gen.copy()
+    data_gen[:, :, 1] -= height_offset_gen
+
+    def update_single(frame, ax, data, data_other, spline_line, lines, trajectory, initialized,lines_elbow_last_point,lines_elbow_last_point_other,index_for_joint):
+        #ax.set_xlim3d([-radius/2 + trajectory[frame, 0], radius/2 + trajectory[frame, 0]])
+        #ax.set_ylim3d([-radius/2 + trajectory[frame, 1], radius/2 + trajectory[frame, 1]])
+
+        positions_world = data[frame]
+        positions_world_other = data_other[frame]
+        for i in range(positions_world.shape[0]):
+            if skeleton_parents[i] == -1:
+                continue
+            if not initialized:
+                col = 'red' if i in skeleton.joints_right() else 'black' # As in audio cables :)
+                lines.append(ax.plot([positions_world[i, x], positions_world[skeleton_parents[i], x]],
+                        [positions_world[i, y], positions_world[skeleton_parents[i], y]],
+                        [positions_world[i, z], positions_world[skeleton_parents[i], z]], zdir='y', c=col))
+            else:
+                lines[i-1][0].set_xdata([positions_world[i, x], positions_world[skeleton_parents[i], x]])
+                lines[i-1][0].set_ydata([positions_world[i, y], positions_world[skeleton_parents[i], y]])
+                lines[i-1][0].set_3d_properties([positions_world[i, z], positions_world[skeleton_parents[i], z]], zdir='y')
+        if not initialized:
+            lines_elbow_last_point[0] = [positions_world[index_for_joint,x],positions_world[index_for_joint,y],positions_world[index_for_joint,z]]
+            lines_elbow_last_point_other[0] = [positions_world_other[index_for_joint,x],positions_world_other[index_for_joint,y],positions_world_other[index_for_joint,z]]
+        else:
+            ax.plot([lines_elbow_last_point[0][0],positions_world[index_for_joint,x]],
+                [lines_elbow_last_point[0][1],positions_world[index_for_joint,y]],
+                [lines_elbow_last_point[0][2],positions_world[index_for_joint,z]],zdir='y',c='blue',linewidth=0.5)
+            ax.plot([lines_elbow_last_point_other[0][0],positions_world_other[index_for_joint,x]],
+                [lines_elbow_last_point_other[0][1],positions_world_other[index_for_joint,y]],
+                [lines_elbow_last_point_other[0][2],positions_world_other[index_for_joint,z]],zdir='y',c='red',linewidth=0.5)
+            lines_elbow_last_point[0] = [positions_world[index_for_joint,x],positions_world[index_for_joint,y],positions_world[index_for_joint,z]]
+            lines_elbow_last_point_other[0] = [positions_world_other[index_for_joint,x],positions_world_other[index_for_joint,y],positions_world_other[index_for_joint,z]]
+
+
+        l = max(frame-draw_offset, 0)
+        r = min(frame+draw_offset, trajectory.shape[0])
+        spline_line.set_xdata(trajectory[l:r, 0])
+        spline_line.set_ydata(np.zeros_like(trajectory[l:r, 0]))
+        spline_line.set_3d_properties(trajectory[l:r, 1], zdir='y')
+        if output == 'interactive' and frame == data.shape[0] - 1:
+            plt.close('all')
+
+    def update(frame):
+        nonlocal initialized_real, lines_real_elbow_last_point, index_for_joint
+        nonlocal initialized_gen, lines_gen_elbow_last_point
+        update_single(frame, ax_real, data_real, data_gen, spline_line_real, lines_real, trajectory_real, initialized_real, lines_real_elbow_last_point, lines_gen_elbow_last_point, index_for_joint)
+        update_single(frame, ax_gen, data_gen, data_real, spline_line_gen, lines_gen, trajectory_gen, initialized_gen, lines_gen_elbow_last_point, lines_real_elbow_last_point, index_for_joint)
+
+        initialized_real = True
+        initialized_gen = True
+
+    fig.tight_layout()
+    num_frames = np.min([data_real.shape[0], data_gen.shape[0]])
+    anim = FuncAnimation(fig, update, frames=np.arange(0, num_frames), interval=1000/fps, repeat=False)
+    if output == 'interactive':
+        plt.show()
+        return anim
+    elif output == 'html':
+        return anim.to_html5_video()
+    elif output.endswith('.mp4'):
+        Writer = writers['ffmpeg']
+        writer = Writer(fps=fps, metadata={}, bitrate=bitrate)
+        anim.save(output, writer=writer)
+    elif output.endswith('.gif'):
+        anim.save(output, dpi=80, writer='imagemagick')
+    else:
+        raise ValueError('Unsupported output format (only html, .mp4, and .gif are supported)')
+    plt.close()
